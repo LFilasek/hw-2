2e6493b171d8e33fceeed58fe4d823cf1dd7aac1
Yiannis Voulgaridis <yiannisvoulgaridis1@gmail.com>
2026-09-24 20:37:32 +0000

    Character check 4.0

--- main.cpp
+++ main.cpp
@@ -40,18 +40,28 @@ int main( int argc, char * argv[] )
 			i++;
 		}
 	}
-	for (int i = 0; i < 4; i++){
-		if(!isdigit(argv[i])){
-			cout<<"invalid input"<<endl;
-			return -1;
-		}
-	}
+	
 
 	loan_amount = arguments[0];
+
+	if (!isdigit(loan_amount)){
+		cout<<"invalid input"<<endl;
+		return -1;
+	}
 	
 	yearly_interest_rate = arguments[1];
+
+	if (!isdigit(yearly_interest_rate)){
+		cout<<"invalid input"<<endl;
+		return -1;
+	}
 	
 	monthly_payment = arguments[2];
+
+	if (!isdigit(monthly_payment)){
+		cout<<"invalid input"<<endl;
+		return -1;
+	}
 	cout << loan_amount << " " << yearly_interest_rate << " " << monthly_payment << endl;
 
 
